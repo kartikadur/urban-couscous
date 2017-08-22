2db9cd167d0a0bbf8a1fd42f99b66f28d9c335b6
kartik adur <kartikadur@gmail.com>
2017-08-22 19:49:06 +0530

    Remove classes
    Also added karma and unit/functional testing modules

--- app/parts/loader/index.js
+++ app/parts/loader/index.js
@@ -3,9 +3,6 @@ import Mustache from 'mustache';
 import template from './loader.html';
 import './loader.scss';
 
-export default class Loader {
-  static render() {
-    const body = document.getElementsByTagName('body')[0];
-    body.innerHTML = Mustache.render(template);
-  }
+export default function loader() {
+  return Mustache.render(template);
 }
